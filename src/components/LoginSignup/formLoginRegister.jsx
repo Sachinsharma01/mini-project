e0f3889363a78dfddf754f9a1693fe2f584a6112
Pragya Sharma <67971009+pragya-sharma11@users.noreply.github.com>
2021-11-08 20:26:07 +0530

    Create formLoginRegister.jsx

---
+++ src/components/LoginSignup/formLoginRegister.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react/cjs/react.development";
+import Login from "./login";
+import Register from "./register";
+import './style.css'
+const LoginRegister=()=>{
+    const [slide, setSlide] = useState(false)
+    return(
+        <div className='contain'>
+            <div className='side'>
+            <p className='text'>Experience the best connections with your friends and family.</p>
+            <img src="./photo/mini.JPG" alt="" />
+            <div className='btndiv'>
+            <button className='change' onClick={()=>{
+                if(slide){
+                setSlide(false)}
+                else{
+                setSlide(true)}
+            }} >{!slide?'Login if already registered.':"Don't have an account.Register please."}</button>
+            </div>
+        </div>
+            <div>{slide?< Login/>:< Register/>}</div>
+        </div>
+    )
+}
+export default LoginRegister
