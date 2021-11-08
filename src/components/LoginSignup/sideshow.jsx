8f8baea846db131479db772d12980fd41cf99cd6
Pragya Sharma <67971009+pragya-sharma11@users.noreply.github.com>
2021-11-08 21:02:29 +0530

    Create sideshow.jsx

---
+++ src/components/LoginSignup/sideshow.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import './style.css'
+//live share ext-
+function sideshow(){
+   return(
+    <div className='side'>
+    <p className='text'>Experience the best connections with your friends and family.</p>
+    <div className='btndiv'>
+    <button className='change'>Login if already registered.</button>
+    </div>
+</div>
+    )
+}
+export default sideshow;
