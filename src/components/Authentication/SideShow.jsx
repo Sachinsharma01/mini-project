d5b13a4f72a7d3b9a51275bda6519e4a7ed6754e
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-11 01:30:19 +0530

    put photos

--- src/components/Authentication/SideShow.jsx
+++ src/components/Authentication/SideShow.jsx
@@ -5,7 +5,7 @@ function SideShow(props){
    return(
     <div className='side'>
             <p className='text'>Experience the best connections with your friends and family.</p>
-            <img src='./photo/mini.JPG' />
+            <img src='./mini.JPG' />
             <div className='btndiv'>
                 <button className='button' onClick={props.onClickSetSlide} >{!props.slide?'Login if already registered.':"Don't have an account.Register"}</button>
             </div>
