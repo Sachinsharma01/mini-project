d010bfeda1d891d9497c0cfe75ca83a458f63108
nishant mishra <nishant.mishra_cs19@gla.ac.in>
2021-11-16 22:17:11 +0530

    input

---
+++ src/components/ChatInput/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ChatInput.css'
+
+const ChatInput = () => {
+    return (
+        <div className="input">
+            <input type="text"></input>
+        </div>
+    )
+}
+
+export default ChatInput
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import ChatInput from '../ChatInput/Index'
 import "./Main.css"
 
 const Main = () => {
     return (
         <div>
-            
+            <ChatInput />
         </div>
     )
 }
